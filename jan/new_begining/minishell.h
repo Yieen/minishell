c79e0572537dc9601a764b9865d704f9a6bf31ca
Ivan Novomlinskiy <inovomli@1-D-6.42heilbronn.de>
2023-02-15 19:20:17 +0100

    added lexer and parser

---
+++ jan/new_begining/minishell.h
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inovomli <inovomli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 14:57:33 by inovomli          #+#    #+#             */
+/*   Updated: 2023/02/15 19:01:34 by inovomli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+typedef struct Lexer
+{
+	int		s_cnt;	// counter for string
+	int		l_cnt;	// new word (lexem) counter
+	int		st_nlm;	// start nem lexem
+	int		pipe_cnt;
+
+	int		sng_qut;
+	int		dub_qut;
+	int		dollar;
+	char	*command;
+}	t_lexer;
+
+typedef struct Shell
+{
+	char	**env_param;
+	char	**lexer_res;
+	char	*prompt;
+	int		cmnd_cnt; // amoun lexer array (now pipe_cnts+1)
+	int		cont_wrk;
+	int		pipe_cnts;
+	char	***parser_res;
+}	t_shell;
+
+typedef struct Environment
+{
+	char	*key;
+	char	*value;
+	char	*str;
+	struct Environment	*next;
+	struct Environment	*prev;
+}	t_env_stract;
+
+#endif
