2e14e4f76bad132e7881cfb8cc5ccbedbb33c275
Ivan Novomlinskiy <inovomli@1-C-5.42heilbronn.de>
2023-02-27 18:30:24 +0100

    chesk leaks

--- jan/new_begining/parser_and_add.c
+++ jan/new_begining/parser_and_add.c
@@ -6,7 +6,7 @@
 /*   By: inovomli <inovomli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 16:06:25 by inovomli          #+#    #+#             */
-/*   Updated: 2023/02/26 16:52:43 by inovomli         ###   ########.fr       */
+/*   Updated: 2023/02/27 18:17:51 by inovomli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	locate_parser_mem(t_shell *shell)
 	int	i;
 
 	i = 0;
-	shell->parser_res = (char ***)malloc(sizeof(char **) * (shell->pipe_cnts + 2));
-	while (i < shell->pipe_cnts + 2)
+	shell->parser_res = (char ***)malloc(sizeof(char **) * (shell->pipe_cnts + 1));
+	while (i < shell->pipe_cnts + 1)
 	{
 		shell->parser_res[i] = (char **) malloc(sizeof(char *) * (1024));
 		i++;
@@ -64,24 +64,16 @@ char	**del_elms_fr_array(char **src, int size, int *del_arr)
     dest = malloc(sizeof(char *) * (size + 1));
 	while (src[i])
 	{
-		// printf("parsre_res %d %s;\n",i, src[i]);
 		j = 0;
-		// printf("%d %d\n", del_arr[0], del_arr[1]);
 		copy = 1;
 		while (del_arr[j])
 		{
-			// printf("del %d\n", del_arr[j]);
 			if (i == del_arr[j])
 				copy = 0;
 			j++;
 		}
 		if (copy)
-		{
-			dest[new_cnt] = malloc((ft_strlen(src[i]) + 1) * sizeof(char));
-			ft_strlcpy(dest[new_cnt], src[i], ft_strlen(src[i]) + 1);
-						// printf("dest %d %s;\n",new_cnt, dest[new_cnt]);
-			new_cnt++;			
-		}
+			dest[new_cnt++] = src[i];
 		i++;
     }
 	dest[new_cnt] = 0;
@@ -101,25 +93,20 @@ void	post_parser(t_shell *shell)
 	while (shell->parser_res[i])
 	{
 		j = 0;
-		// printf("%s\n", shell->parser_res[i][j]);	
 		shell->auxilar[i] = malloc(sizeof(t_pipex));
 		shell->auxilar[i]->is_exec = 1;
 		shell->auxilar[i]->input_fd = -2;
 		shell->auxilar[i]->output_fd = -2;
-		save_pos = malloc(sizeof(int) * 1024);
-		// printf("i=%d\n", i);		
+		save_pos = malloc(sizeof(int) * 1024);	
 		s_p_cnt = 0;					
 		while (shell->parser_res[i][j])
 		{
-			
-			// printf("i=%d j=%d %s;\n",i, j, shell->parser_res[i][j]);
 			if (shell->parser_res[i][j][0] == '>')
 			{
 				if (shell->parser_res[i][j][1] != '>')
 				{
 					if (shell->parser_res[i][j + 1] != NULL)
 					{
-						// printf("%d\n", j+1);
 						if (shell->auxilar[i]->input_fd != -2)
 							close(shell->auxilar[i]->input_fd);
 						if (!is_sp_sim(shell->parser_res[i][j + 1][0]))
@@ -131,7 +118,6 @@ void	post_parser(t_shell *shell)
 						}
 						if (shell->auxilar[i]->input_fd == -1)
 							shell->auxilar[i]->is_exec = 0;
-						// save j
 						save_pos[s_p_cnt] = j;
 						save_pos[s_p_cnt + 1] = j + 1;
 						s_p_cnt += 2;	
@@ -141,7 +127,6 @@ void	post_parser(t_shell *shell)
 				{
 					if (shell->parser_res[i][j + 1] != NULL)
 					{
-						// printf("%d\n", j+1);
 						if (shell->auxilar[i]->input_fd != -2)
 							close(shell->auxilar[i]->input_fd);
 						if (!is_sp_sim(shell->parser_res[i][j + 1][0]))							
@@ -153,7 +138,6 @@ void	post_parser(t_shell *shell)
 						}	
 						if (shell->auxilar[i]->input_fd == -1)
 							shell->auxilar[i]->is_exec = 0;
-						// save j
 						save_pos[s_p_cnt] = j;
 						save_pos[s_p_cnt + 1] = j + 1;
 						s_p_cnt += 2;							
@@ -166,7 +150,6 @@ void	post_parser(t_shell *shell)
 				{
 					if (shell->parser_res[i][j + 1] != NULL)
 					{
-						// printf("%d\n", j+1);
 						if (shell->auxilar[i]->output_fd != -2)
 							close(shell->auxilar[i]->output_fd);
 						if (!is_sp_sim(shell->parser_res[i][j + 1][0]))								
@@ -178,7 +161,6 @@ void	post_parser(t_shell *shell)
 						}	
 						if ((shell->auxilar[i]->output_fd == -1) || (is_sp_sim(shell->parser_res[i][j + 1][0])))
 							shell->auxilar[i]->is_exec = 0;	
-						// save j
 						save_pos[s_p_cnt] = j;
 						save_pos[s_p_cnt + 1] = j + 1;
 						s_p_cnt += 2;							
@@ -188,7 +170,6 @@ void	post_parser(t_shell *shell)
 				{
 					if (shell->parser_res[i][j + 1] != NULL)
 					{
-						// printf("%d\n", j+1);
 						if (shell->auxilar[i]->output_fd != -2)
 							close(shell->auxilar[i]->output_fd);
 						if (!is_sp_sim(shell->parser_res[i][j + 1][0]))	
@@ -200,7 +181,6 @@ void	post_parser(t_shell *shell)
 						}	
 						if ((shell->auxilar[i]->output_fd == -1) || (is_sp_sim(shell->parser_res[i][j + 1][0])))
 							shell->auxilar[i]->is_exec = 0;	
-						// save j
 						save_pos[s_p_cnt] = j;
 						save_pos[s_p_cnt + 1] = j + 1;
 						s_p_cnt += 2;							
@@ -208,21 +188,11 @@ void	post_parser(t_shell *shell)
 				}									
 			}
 			j++;
-			
-			// printf("%d\n", j);
 		}
-		save_pos[s_p_cnt] = 0;
-		// for (int k = 0; k < 6; k++)
-		// {
-		// 	printf("save_pos %d\n",save_pos[k]);
-		// }
-		// del j, j+1
+		save_pos[s_p_cnt] = 0;		
 		size = twodimarr_str_calc(shell->parser_res[i]);
 		interim = del_elms_fr_array(shell->parser_res[i], size, save_pos);
-		// for (int k = 0; k < j; k++)
-		// {
-		// 	printf("interim %d %s;\n",k, interim[k]);
-		// }
+		free(shell->parser_res[i]);
 		shell->parser_res[i] = interim;
 		i++;
 		free(save_pos);
